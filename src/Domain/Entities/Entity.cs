dcc068ef6a4a8d894ded556df232a490447c435e
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2023-12-13 19:07:08 -0300

    refactor: creating entity class

---
+++ src/Domain/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public abstract class Entity
+    {
+        public  Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
